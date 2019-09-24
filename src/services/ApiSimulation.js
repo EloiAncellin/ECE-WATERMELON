c84450836f0c63f04ca6b20ddb42c1ace4cb6600
Omar Aflak <aflakomar@gmail.com>
2019-09-24 23:05:38 +0200

    fixed method access

--- src/services/ApiSimulation.js
+++ src/services/ApiSimulation.js
@@ -1,18 +1,18 @@
 import {users, cards, wallets} from './MockObjects.js'
 
 export class ApiSimulation{
-    function wait(ms){
+    static wait(ms){
         Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
     }
 
-    function success(result){
+    static success(result){
         return JSON.stringify({
             status: "success",
             result: result
         })
     }
 
-    function failure(error){
+    static failure(error){
         return JSON.stringify({
             status: "failure",
             error: error
@@ -20,22 +20,24 @@ export class ApiSimulation{
     }
 
     static authenticateUser(email, password){
-        wait(1000)
+        ApiSimulation.wait(1000)
         for(user of users){
             if(user.email == email && user.password == password){
-                return success(user)
+                return ApiSimulation.success(user)
             }
         }
-        return failure("user not found, or wrong password")
+        return ApiSimulation.failure("user not found, or wrong password")
     }
 
     static getWallet(userId){
-        wait(1500)
+        ApiSimulation.wait(1500)
         for(wallet of wallets){
             if(wallet.userId == userId){
-                return success(wallet)
+                return ApiSimulation.success(wallet)
             }
         }
-        return failure("user has no wallet")
+        return ApiSimulation.failure("user has no wallet")
     }
+
+    static
 }
